218081019e85962ca0db946dbd693588d57f7557
Beverly M <bevponi@gmail.com>
2023-03-18 18:15:06 +0000

    Terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+var ext = [".com", ".net", ".us", ".io"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (i = 0; i < pronoun.length; i++) {
+  for (k = 0; k < adj.length; k++) {
+    for (z = 0; z < noun.length; z++) {
+      for (e = 0; e < ext.length; e++) {
+        console.log(pronoun[i] + adj[k] + noun[z] + ext[e]);
+      }
+    }
+  }
+}
